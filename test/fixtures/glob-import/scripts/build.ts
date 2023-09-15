0ca7f33d5f6e0aed1fb9542b5556bf664464fbf2
Leon Si <leon@leondreamed.com>
2023-09-14 22:28:47 -0400

    fix

--- test/fixtures/glob-import/scripts/build.ts
+++ test/fixtures/glob-import/scripts/build.ts
@@ -3,7 +3,7 @@ import { getMonorepoDirpath } from "get-monorepo-root";
 import esbuild from "esbuild";
 
 esbuild.build({
-  entrypoints: ["../src/index.ts"],
+  entryPoints: ["../src/index.ts"],
   outdir: "../dist",
   plugins: [
     globImportsPlugin({ monorepoDirpath: getMonorepoDirpath(__dirname) }),
